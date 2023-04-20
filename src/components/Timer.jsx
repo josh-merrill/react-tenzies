ab099d48d035238c005960d013c18234ef72801a
Josh Merrill <joshmmerrill@outlook.com>
2023-04-20 14:46:16 +0100

    FIX: Small update to timer component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 
 export default function Timer({ tenzies }) {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(0)
 
   useEffect(() => {
-    let interval;
+    let interval
     if (!tenzies) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
+        setTime((prevTime) => prevTime + 10)
+      }, 10)
     }
-    return () => clearInterval(interval);
-  }, [tenzies]);
+    return () => clearInterval(interval)
+  }, [tenzies])
 
   const formatTime = (time) => {
-    const minutes = Math.floor(time / 60000);
-    const seconds = Math.floor((time % 60000) / 1000);
-    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  };
+    const minutes = Math.floor(time / 60000)
+    const seconds = Math.floor((time % 60000) / 1000)
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+  }
 
   return (
   <h2 className="dice-timer">{formatTime(time)}</h2>
