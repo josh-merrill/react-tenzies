3ea7e06172fc339afdac51716692e70040c6237b
Josh Merrill <joshmmerrill@outlook.com>
2023-04-20 14:03:42 +0100

    FEAT: Created and implemented timer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,63 @@
-import Reac, { useState, useEffect } from "react";
-import Die from "./Die";
-import Dicecount from "./Dicecount";
-import { nanoid } from "nanoid";
-import Confetti from "react-confetti";
-import Star from "./assets/star.png";
+import Reac, { useState, useEffect } from "react"
+import Die from "./Die"
+import Dicecount from "./Dicecount"
+import Timer from "./Timer"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+// import Star from "./assets/star.png"
 
 export default function App() {
-  const [dice, setDice] = useState(allNewDice());
-  const [tenzies, setTenzies] = useState(false);
-  const [count, setCount] = useState(0);
+  const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
-    const allHeld = dice.every((die) => die.isHeld);
-    const firstValue = dice[0].value;
-    const allSameValue = dice.every((die) => die.value === firstValue);
+    const allHeld = dice.every((die) => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every((die) => die.value === firstValue)
     if (allHeld && allSameValue) {
-      setTenzies(true);
+      setTenzies(true)
     }
-  }, [dice]);
+  }, [dice])
 
   function generateNewDie() {
     return {
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
       id: nanoid(),
-    };
+    }
   }
 
   function allNewDice() {
-    const newDice = [];
-    for (let i = 0; i < 10; i++) {
-      newDice.push(generateNewDie());
+    const newDice = []
+    for (let i = 0 i < 10 i++) {
+      newDice.push(generateNewDie())
     }
-    return newDice;
+    return newDice
   }
 
   function rollDice() {
     if (!tenzies) {
-      setCount(count + 1);
-      console.log(count);
+      setCount(count + 1)
+      console.log(count)
       setDice((oldDice) =>
         oldDice.map((die) => {
-          return die.isHeld ? die : generateNewDie();
+          return die.isHeld ? die : generateNewDie()
         })
-      );
+      )
     } else {
-      setCount(0);
-      setTenzies(false);
-      setDice(allNewDice());
+      setCount(0)
+      setTenzies(false)
+      setDice(allNewDice())
     }
   }
 
   function holdDice(id) {
     setDice((oldDice) =>
       oldDice.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die
       })
-    );
+    )
   }
 
   const diceElements = dice.map((die) => (
@@ -66,13 +67,13 @@ export default function App() {
       isHeld={die.isHeld}
       holdDice={() => holdDice(die.id)}
     />
-  ));
+  ))
 
   return (
-    <div className="tenzies-wrapper">
-      <div className="image-top-star">
-        <img src={Star} />
-      </div>
+    // <div className="tenzies-wrapper">
+    //   <div className="image-top-star">
+    //     <img src={Star} />
+    //   </div>
       <main>
         {tenzies && <Confetti />}
         <div className="title-container">
@@ -83,16 +84,19 @@ export default function App() {
             Roll until all dice are the same. Click each die to freeze it at its
             current value between rolls.
           </p>
-          <Dicecount count={count} />
+          <div className="dice-count-container">
+            <Timer tenzies={tenzies} />
+            <Dicecount count={count} />
+          </div>
           <div className="dice-container">{diceElements}</div>
           <button className="roll-dice" onClick={rollDice}>
             {tenzies ? "New Game" : "Roll"}
           </button>
         </div>
       </main>
-      <div className="image-bottom-star">
-            <img src={Star} />
-      </div>
-    </div>
-  );
+    //   <div className="image-bottom-star">
+    //     <img src={Star} />
+    //   </div>
+    // </div>
+  )
 }
---
+++ src/Timer.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+export default function Timer({ tenzies }) {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (!tenzies) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [tenzies]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  return (
+  <h2 className="dice-timer">{formatTime(time)}</h2>
+  )
+}
